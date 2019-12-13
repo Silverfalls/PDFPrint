e8d8649bfea421f6e52b5f8b044b18a401652de1
Alexander Schröders <alexander.schroeders@googlemail.com>
2019-12-13 20:17:58 +0100

    Projektdateien hinzufügen.

---
+++ PDFPrint/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Syncfusion.Pdf.Parsing;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
+
+namespace PDFPrint
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Opens a file picker.
+            var picker = new FileOpenPicker();
+            picker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            picker.ViewMode = PickerViewMode.List;
+            //Filters PDF files in the documents library.
+            picker.FileTypeFilter.Add(".pdf");
+            var file = await picker.PickSingleFileAsync();
+            if (file == null) return;
+            //Reads the stream of the loaded PDF document.
+            var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
+            Stream fileStream = stream.AsStreamForRead();
+            byte[] buffer = new byte[fileStream.Length];
+            fileStream.Read(buffer, 0, buffer.Length);
+            //Loads the PDF document.
+            PdfLoadedDocument loadedDocument = new PdfLoadedDocument(buffer);
+            pdfViewer.LoadDocument(loadedDocument);
+        }
+
+        private async void pdfViewer_DocumentLoaded(object sender, Syncfusion.Windows.PdfViewer.DocumentLoadedEventArgs e)
+        {
+            await Task.Delay(100);
+            pdfViewer.Print();
+        }
+    }
+}
